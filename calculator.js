2af5813a5c2a617ecba8cf48962034c4d9e265cc
sauravagrawal <saurav.agrawal27@gmail.com>
2024-08-08 18:17:45 +0530

    test case added for sum of two numbers separated by a comma

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  return parseInt(numbers, 10);
+
+  const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = { add };
--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,7 @@ test("returns 0 for an empty string", () => {
 test("returns the number for a single number string", () => {
   expect(calculator.add("1")).toBe(1);
 });
+
+test("returns the sum of two numbers separated by a comma", () => {
+  expect(calculator.add("1,5")).toBe(6);
+});
