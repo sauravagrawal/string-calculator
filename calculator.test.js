f0e7eee2cc820716b563c58c62c72cbc90929cb8
sauravagrawal <saurav.agrawal27@gmail.com>
2024-08-08 18:19:55 +0530

    test case added for sum of multiple numbers separated by commas

--- calculator.test.js
+++ calculator.test.js
@@ -11,3 +11,7 @@ test("returns the number for a single number string", () => {
 test("returns the sum of two numbers separated by a comma", () => {
   expect(calculator.add("1,5")).toBe(6);
 });
+
+test("returns the sum of multiple numbers separated by commas", () => {
+  expect(calculator.add("1,2,3,4")).toBe(10);
+});
