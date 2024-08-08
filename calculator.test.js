d3ff59284dd66be53669b6827f5f0fa78961caf9
sauravagrawal <saurav.agrawal27@gmail.com>
2024-08-08 18:16:24 +0530

    test case for a simple number string

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require("./calculator");
 test("returns 0 for an empty string", () => {
   expect(calculator.add("")).toBe(0);
 });
+
+test("returns the number for a single number string", () => {
+  expect(calculator.add("1")).toBe(1);
+});
